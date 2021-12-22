3977683df7daada5b15b7a8ec9c8dbc40c17e701
Diego Alberto <diego.alberto@gerencianet.com.br>
2021-12-22 14:30:22 -0300

    feat: request github api

---
+++ app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import requests
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def main():
+    df = requests.get(
+        'https://api.github.com/repos/bacen/pix-dict-api/releases').json()[0]
+
+    for asset in df['assets']:
+        if ".html" in asset['name']:
+            r = requests.get(asset['browser_download_url'],
+                             allow_redirects=True)
+    return r.content
