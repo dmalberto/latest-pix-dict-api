d728e356440106f32da4a1e554974f40fe5d398c
Diego Alberto <diego.alberto@gerencianet.com.br>
2021-12-22 14:51:18 -0300

    feat: add update route

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 from fastapi import FastAPI
 import requests
 from fastapi.responses import HTMLResponse
@@ -15,3 +16,11 @@ async def main():
             r = requests.get(asset['browser_download_url'],
                              allow_redirects=True)
     return r.content
+
+
+@app.get("/update")
+async def main():
+    df = requests.get(
+        'https://api.github.com/repos/bacen/pix-dict-api/releases').json()[0]
+
+    return json.dumps({"status": 200, "version": df['tag_name']})
