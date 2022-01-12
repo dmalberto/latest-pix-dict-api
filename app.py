6fe578666a78f849c035511e613addc5d4c2b5d1
Diego Alberto <dmalberto@outlook.com>
2022-01-12 11:28:11 +0000

    fix: add local content cache

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
-import json
 from fastapi import FastAPI
 import requests
 from fastapi.responses import HTMLResponse
+from pydantic import BaseSettings
 
+
+class Settings(BaseSettings):
+    content: str = ""
+    dict_version: str = ""
+
+
+settings = Settings()
 app = FastAPI()
 
 
 @app.get("/", response_class=HTMLResponse)
 async def main():
-    df = requests.get(
-        'https://api.github.com/repos/bacen/pix-dict-api/releases').json()[0]
-
-    for asset in df['assets']:
-        if ".html" in asset['name']:
-            r = requests.get(asset['browser_download_url'],
-                             allow_redirects=True)
-    return r.content
+    if (not settings.content):
+        await update()
+    return settings.content
 
 
 @app.get("/update")
-async def main():
+async def update():
     df = requests.get(
         'https://api.github.com/repos/bacen/pix-dict-api/releases').json()[0]
+    if settings.dict_version != df['tag_name']:
+        for asset in df['assets']:
+            if ".html" in asset['name']:
+                settings.content = requests.get(asset['browser_download_url'],
+                                                allow_redirects=True).content
+        settings.dict_version = df['tag_name']
 
-    return json.dumps({'status': 200, 'version': df['tag_name']})
+    return '{"status": 200, "version": ' + settings.dict_version + '}'
